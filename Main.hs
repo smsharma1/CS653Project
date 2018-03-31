ef24a243f320a1b440ba04b3f2c1d81692283726
Shubham Sharma <sharma.shubham736@gmail.com>
2018-04-01 00:01:33 +0530

    Exhaustive DPLL done

--- Main.hs
+++ Main.hs
@@ -3,14 +3,19 @@ import System.IO
 import CNFTokens
 import CNFGrammar
 
+data Bintree a = Node a (Bintree a) (Bintree a) | SAT | UNSAT deriving Show
+
 type Variables = Int
 type Clauses = [Int]
 
+remove :: Eq a => a -> [a] -> [a]
+remove element list = filter (\e -> e/=element) list
 
+literal :: Int -> CNFFormula -> Int
 literal v (Literal a) = if v < a then error "Parsing Error" else a
 literal v (Negative (Literal a)) = if v < a then error "Parsing Error" else -a
 
-
+getclause :: Int -> CNFFormula -> [Int]
 getclause v (Clause a) = getclause v a
 getclause v (UnitClause a) = [literal v a]
 getclause v (PartialClause a b) = [literal v a, literal v b]
@@ -25,7 +30,23 @@ getclauses v n (PartialFormulaJoined a b) = getclause v a:(getclauses v (n-1) b)
 generate :: CNFFormula -> ([Clauses] , [Variables])
 generate (Formula v c t) = ( getclauses v c t, [1..v])
 
+poselem :: (Eq a, Foldable t) => [t a] -> a -> [t a]
+poselem c v = [k | k <- c , not (elem v k)]
+
+decisionpos_up :: (Eq a, Foldable t) => ([t a], [a]) -> ([t a], [a])
+decisionpos_up (c,v)         =      (poselem c (head v), drop 1 v)
+
+negelem :: Eq a => [[a]] -> a -> [[a]]
+negelem c v = map (remove v) c
+
+decisionneg_up (c,v)         =      (negelem c (head v), drop 1 v)
+
+
+dpll (c,v)  | elem [] c      =   UNSAT
+            | v == []        =   SAT
+            | otherwise      =   Node (head v) (dpll (decisionpos_up (c,v))) (dpll (decisionneg_up (c,v)))
+
 main :: IO()
 main = do
     cnftext <- getContents
-    print $ generate $ parseCNF (scanTokens cnftext)
+    print $ dpll $ generate $ parseCNF (scanTokens cnftext)
