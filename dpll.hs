8f89c3ed6643f617eabc90b0df6f22843f79d558
Rahul Gupta <grahul@iitk.ac.in>
2018-04-15 20:26:46 +0530

    added pure literal elimination

--- dpll.hs
+++ dpll.hs
@@ -7,6 +7,8 @@ import Data.List as L
 type Variables = Int
 type Clauses = [Int]
 
+data Status = SAT | UNSAT deriving Show
+
 remove :: Eq a => a -> [a] -> [a]
 remove element list = filter (\e -> e/=element) list
 
@@ -36,9 +38,21 @@ clauseElem cs v = [(remove (-1*v) k) | k <- cs , not (elem v k)]
 generateforvo :: CNFFormula -> [Variables] -> ([Clauses], [Variables])
 generateforvo (Formula v c t) v1 = (getclauses v c t, v1)
 
-decision_up (cs,v)  | elem [] cs    =   False
-                    | v == []       =   True
-                    | otherwise     =   unitPropogate (cs,v) (findsinglevar cs)
+isVPureLit (cs,v) = (or (map (elem v) cs)) /= (or (map (elem (-1 * v)) cs))
+
+findFirstPureLit (cs,[]) = 0
+findFirstPureLit (cs,(v:vs)) | (isVPureLit (cs,v)) = v
+                        | otherwise = findFirstPureLit (cs,vs)
+
+pureLitElem (cs,vs) = takeDecision (cs,vs) (findFirstPureLit (cs,vs))
+
+takeDecision (cs, vs) decV | decV==0 = dpll (cs,vs)
+                        | otherwise = pureLitElem ((clauseElem cs decV), L.delete (abs decV) vs)
+
+
+decision_up (cs,vs)  | elem [] cs    =   False
+                    | vs == []       =   True
+                    | otherwise     =   unitPropogate (cs,vs) (findsinglevar cs)
 
 -- finds var present in single length clause from clauses if any
 findsinglevar [] = 0
@@ -46,17 +60,18 @@ findsinglevar (c:cs) | (length c) == 1  =   (head c)
                      | otherwise        =   findsinglevar cs
 
 -- check if a single variable clause is present, and further call decision on that variable decV
-unitPropogate (cs,v) decV   | decV == 0 =   dpll (cs,v)
-                            | decV > 0  =   decision_up (clauseElem cs decV, L.delete (abs decV) v)
-                            | otherwise =   decision_up (clauseElem cs decV, L.delete (abs decV) v)
+unitPropogate (cs,vs) decV   | decV == 0 =   pureLitElem (cs,vs)
+                            | otherwise =   decision_up (clauseElem cs decV, L.delete (abs decV) vs)
 
--- Returns at first True due to lazy evaluation
-dpll (cs,v)  | elem [] cs    =   False
-            | v == []        =   True
-            | otherwise      =   (decision_up (clauseElem cs (head v), tail v)) || (decision_up (clauseElem cs (-1*(head v)), tail v))
+-- returns at first True due to lazy evaluation
+dpll (cs,vs)  | elem [] cs    =   False
+            | vs == []        =   True
+            | otherwise      =   (decision_up (clauseElem cs (head vs), tail vs)) || (decision_up (clauseElem cs (-1*(head vs)), tail vs))
 
+dpllStatus (cs,vs) | (dpll (cs,vs)) = SAT
+                   | otherwise = UNSAT
 
 main :: IO()
 main = do
     cnftext <- getContents
-    print $ dpll $ generate $ parseCNF (scanTokens cnftext)
+    print $ dpllStatus $ generate $ parseCNF (scanTokens cnftext)
