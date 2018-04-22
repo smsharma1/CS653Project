99e194708f392293f01dad2b15072d7abc59638e
Rahul Gupta <grahul@iitk.ac.in>
2018-04-23 04:03:53 +0530

    incomplete vsids

---
+++ dpll_vsids.hs
@@ -0,0 +1,106 @@
+module Main where
+import System.IO
+import CNFTokens
+import CNFGrammar
+import Data.List as L
+import Prelude hiding (lookup)
+import Data.Map
+import Debug.Trace
+
+type Variables = Int
+type Clauses = [Int]
+
+data Status = SAT | UNSAT deriving Show
+
+remove :: Eq a => a -> [a] -> [a]
+remove element list = Prelude.filter (\e -> e/=element) list
+
+literal :: Int -> CNFFormula -> Int
+literal v (Literal a) = if v < a then error "Parsing Error" else a
+literal v (Negative (Literal a)) = if v < a then error "Parsing Error" else -a
+
+getclause :: Int -> CNFFormula -> [Int]
+getclause v (Clause a) = getclause v a
+getclause v (UnitClause a) = [literal v a]
+getclause v (PartialClause a b) = [literal v a, literal v b]
+getclause v (PartialClauseJoined a b) = literal v a : getclause v b
+
+getclauses :: Int -> Int -> CNFFormula -> [Clauses]
+getclauses v 0 _ = error "Parsing Error"
+getclauses v 1 (PartialFormula a b) = error "Parsing Error"
+getclauses v 2 (PartialFormula a b) = [getclause v a, getclause v b]
+getclauses v n (PartialFormulaJoined a b) = getclause v a:(getclauses v (n-1) b)
+
+generate :: CNFFormula -> ([Clauses] , [Variables])
+generate (Formula v c t) = ( getclauses v c t, [1..v])
+
+-- removes clauses containing dec var and removes neg dec var from clauses
+clauseElem :: (Eq a, Num a) => [[a]] -> a -> [[a]]
+clauseElem cs v = [(remove (-1*v) k) | k <- cs , not (elem v k)]
+
+generateforvo :: CNFFormula -> [Variables] -> ([Clauses], [Variables])
+generateforvo (Formula v c t) v1 = (getclauses v c t, v1)
+
+isVPureLit (cs,v) = (or (Prelude.map (elem v) cs)) /= (or (Prelude.map (elem (-1 * v)) cs))
+
+findFirstPureLit (cs,[]) = 0
+findFirstPureLit (cs,(v:vs)) | (isVPureLit (cs,v)) = v
+                        | otherwise = findFirstPureLit (cs,vs)
+
+pureLitElem (cs,vs) ocs = takeDecision (cs,vs) (findFirstPureLit (cs,vs)) ocs
+
+takeDecision (cs, vs) decV ocs | decV==0 = dpll (cs,vs)
+                            | otherwise = (\(sat, ass) -> (sat, (decV:ass))) (pureLitElem ((clauseElem cs decV), L.delete (abs decV) vs) ocs)
+
+
+decision_up (cs,vs) ocs | elem [] cs    =   (False, [])
+                    | vs == []       =  (True, [])
+                    | otherwise     =  unitPropogate (cs,vs) (findsinglevar cs) ocs
+
+-- finds var present in single length clause from clauses if any
+findsinglevar [] = 0
+findsinglevar (c:cs) | (length c) == 1  =   (head c)
+                     | otherwise        =   findsinglevar cs
+
+-- check if a single variable clause is present, and further call decision on that variable decV
+unitPropogate (cs,vs) decV ocs | decV == 0 =   pureLitElem (cs,vs) ocs
+                            | otherwise =  findConflictClause ((\(sat, ass) -> (sat, (decV:ass))) (decision_up (clauseElem cs decV, L.delete (abs decV) vs) ocs)) ocs
+
+findConflictClause (sat1, ass1) ocs = case sat1 of
+                                        True -> (sat1, ass1)
+                                        False -> 
+                                            -- enumerate all original clauses which have become unsat
+                                            -- resolve them to get conflict clause
+
+-- returns at first True due to lazy evaluation
+startdpll (cs,vs) ocs  | elem [] cs    =   (False, [])
+                    | vs == []        =   (True, [])
+                    | otherwise      =  branchLiteral decV (decision_up (clauseElem cs decV, L.delete (abs decV) vs) ocs) (decision_up (clauseElem cs (-1*decV), L.delete (abs decV) vs) ocs)
+                                where decV = chooseLit (cs,vs) 
+
+branchLiteral decV = \(sat1, ass1) ~(sat2,ass2) -> case sat1 of 
+                                                        True -> (True, decV:ass1)                                                        
+                                                        False -> case sat2 of
+                                                                    True -> (True, decV:ass2)                                                                    
+                                                                    False -> (False, [])
+
+dpll (cs,vs) = startdpll (cs,vs) cs
+
+-- For DLIS                                               
+createDict (cs, vs) = populate cs (createEmpty vs)
+
+createEmpty vs = fromList [(v, 0) | v<-vs]
+
+populate cs myDict = Prelude.foldr addClauseToDict myDict cs 
+
+addClauseToDict clause newDict = Prelude.foldr (\x -> adjustWithKey (\_-> (+ 1)) (abs x)) newDict clause
+ 
+chooseLit (cs,vs) = fst (foldrWithKey maxTuple (0,0) (createDict (cs,vs)))
+
+maxTuple k a result | (snd result) > a = result
+                    | otherwise = (k,a)
+
+main :: IO()
+main = do
+    cnftext <- getContents
+    print $ dpll $ generate $ parseCNF (scanTokens cnftext)
